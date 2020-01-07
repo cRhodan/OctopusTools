77af425595d06631665d99f82f1f39bea059c28d
Chris Rhodan <crhodan@deloitte.com.au>
2020-01-07 22:19:41 +0000

    Initial Commit

---
+++ VariableImportExport/Options.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine;
+
+namespace VariableImportExport
+{
+    public class Options
+    {
+        [Option('u', "url", Required = true, HelpText = "Url for your Octopus Instance")]
+        public string OctopusUrl { get; set; }
+
+        [Option('k', "apiKey", Required = true, HelpText = "Api Key for your Octopus Instance")]
+        public string OctopusApiKey { get; set; }
+
+        [Option('p', "projectName", Required = true, HelpText = "The Name of the Octopus Project")]
+        public string OctopusProjectName { get; set; }
+
+        [Option('s', "switch", Required = true, HelpText = "Set to export (e) to export variables, set to import (i) to import variables")]
+        public string ImportExportSwitch { get; set; }
+
+        [Option('f', "filePath", Required = false, HelpText = "File path for the folder where the variable json file will be. Defaults to c:\\temp")]
+        public string SuppliedFilePath { get; set; } = @"C:\temp";
+            
+        [Option('n', "fileName", Required = false, HelpText = "File name for the variable json file, including file extension. Defaults to OctopusVariables.json")]
+        public string SuppliedFileName { get; set; } = "OctopusVariables.json";
+    }
+}
---
+++ VariableImportExport/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using CommandLine;
+using Newtonsoft.Json;
+using Octopus.Client;
+using Octopus.Client.Model;
+
+namespace VariableImportExport
+{
+    class Program
+    {
+        private static readonly string[] ExportValues = {"export", "e"};
+        private static readonly string[] ImportValues = {"import", "i"};
+        
+        private static string FullPathToFile { get; set; }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o =>
+                {
+                    FullPathToFile = $"{o.SuppliedFilePath}\\{o.SuppliedFileName}";
+                    Console.WriteLine($"Using {FullPathToFile} as File Path.");
+                    
+                    var endpoint = new OctopusServerEndpoint(o.OctopusUrl, o.OctopusApiKey);
+                    var repository = new OctopusRepository(endpoint);
+
+                    var project = repository.Projects.FindByName(o.OctopusProjectName);
+                    var set = repository.VariableSets.Get(project.VariableSetId);
+
+                    var caseInsensitiveSwitch = o.ImportExportSwitch.ToLower();
+
+                    if (ExportValues.Contains(caseInsensitiveSwitch))
+                    {
+                        HandleExport(set);
+                    }
+
+                    else if (ImportValues.Contains(caseInsensitiveSwitch))
+                    {
+                        HandleImport(set, repository);
+                    }
+
+                    else Console.WriteLine("Invalid switch option provided. Value must be either export (e) or import (i).");
+                });
+        }
+
+        private static void HandleExport(VariableSetResource variableSet)
+        {
+            Console.WriteLine("Beginning Export");
+            File.WriteAllText(FullPathToFile, JsonConvert.SerializeObject(variableSet, Formatting.Indented));
+            Console.WriteLine("Export Complete");
+        }
+
+        private static void HandleImport(VariableSetResource variableSet, OctopusRepository repository)
+        {
+            Console.WriteLine("Beginning Import");
+            
+            var set = JsonConvert.DeserializeObject<VariableSetResource>(File.ReadAllText(FullPathToFile));
+
+            set.Id = variableSet.Id; 
+            set.OwnerId = variableSet.OwnerId;
+            set.Version = variableSet.Version;
+            set.Links = variableSet.Links;
+
+            // Update the variable back into Octopus
+            repository.VariableSets.Modify(set);
+            
+            Console.WriteLine("Import Complete");
+        }
+    }
+}
